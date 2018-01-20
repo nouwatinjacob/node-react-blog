69ab66b108ae6fa6189915063e9b44d95ad5f301
Nouwatin Jacob <jaysansa@gmail.com>
2018-01-20 14:05:52 +0100

    cconnecting to db using config.js

---
+++ server/config/config.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres',
+    operatorsAliases: false
+  },
+  test: {
+    username: process.env.TEST_DB_USER,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_NAME,
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres',
+    operatorsAliases: false
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  }
+};
+
+module.exports = config[process.env.NODE_ENV || 'development'];
