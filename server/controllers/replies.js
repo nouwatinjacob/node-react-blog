5ba516eff3bbc89e0abce54890224d39980e898c
Nouwatin Jacob <jaysansa@gmail.com>
2018-01-23 03:56:25 +0100

    create fnx in reply controller

---
+++ server/controllers/replies.js
@@ -0,0 +1,41 @@
+import db from '../models';
+import Validator from 'validatorjs';
+
+const Review = db.Review;
+const Post = db.Post;
+const Reply = db.Reply;
+
+const replyRule ={
+  reply_body: 'required'
+}
+
+const replyController = {
+  create(req, res) {
+    const body = req.body;
+    const validator = new Validator(body, replyRule);
+    if(validator.fails()) {
+      return res.status(400).json({ code:400, message: validator.errors.all() });
+    }
+    Review.findById(req.params.reviewId)
+    .then((review) => {
+      if(!review){
+        return res.status(404).json({ code:404, message: 'Review not found'});
+      }
+      return Reply.create({
+        reply_body: body.reply_body,
+        review_id: req.params.reviewId,
+        post_id: review.post_id, 
+        user_id: req.decoded.id
+      })
+      .then((savedReply) => {
+        return res.status(201).json({ code: 201, message: 'Reply created', data: savedReply});
+      })
+      .catch(error => res.status(400).json(error));
+    })
+    .catch(error => res.status(400).json(error));
+  },
+  
+
+};
+
+export default replyController;
